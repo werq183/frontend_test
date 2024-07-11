b4b7d755da658cc1f1767962437a5580884e644e
Weronika Kot <97183851+werq183@users.noreply.github.com>
2024-07-11 21:02:33 +0200

    style: add navbar styles

--- recruitmentTask/src/app/navbar/navbar.component.ts
+++ recruitmentTask/src/app/navbar/navbar.component.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    NgIf
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
